70edca880ba1e823979bc2ceb6da8036bfe8864b
hu-sena <shusnazubir@gmail.com>
2023-03-08 10:54:14 +0800

    Added project

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import NavBar from '../../components/navbar/NavBar';
+import Header from '../../components/header/Header';
+import "./home.css"
+
+export const Home = () => {
+  return (
+    <div>
+        <NavBar/>
+        <Header/>
+    </div>
+  )
+}
+
+export default Home
+
